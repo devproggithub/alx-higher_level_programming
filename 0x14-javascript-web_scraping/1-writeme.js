bccc1b3b09ecc00930fec7d1f9e390a66e2795c1
devproggithub <dev.prognft@gmail.com>
2024-04-23 14:26:45 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+// Import the built-in Node.js 'fs' module.
 
-const fileName = process.argv[2];
-const fileContent = process.argv[3];
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
+  // Use fs.writeFile() to write data to a file specified as the third command-line argument (process.argv[2]).
+  // The data to be written is taken from the fourth command-line argument (process.argv[3]).
 
-fs.writeFile(fileName, fileContent, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
+  if (error) {
+    // If an error occurs during the write operation, the 'error' parameter will contain an error object.
+    console.error(error);
   }
 });
